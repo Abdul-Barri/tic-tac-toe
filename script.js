b03fcfd412721bb208a386948f531001a5406b54
Abdul-Barri <lawalabdulbarri@gmail.com>
2022-08-06 13:29:06 +0100

    game logic development

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const gameStatus = document.querySelector(".status");
+const btns = document.querySelectorAll(".btn");
+console.log(btns);
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    if (btns[i].textContent === "") {
+      if (btns[i].textContent !== "X") {
+        btns[i].textContent = "X";
+      } else {
+        btns[i].textContent = "O";
+      }
+    }
+  });
+}
