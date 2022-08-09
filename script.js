a34979aacc79a395fff39e6d12010314819f8305
Abdul-Barri <lawalabdulbarri@gmail.com>
2022-08-09 20:23:26 +0100

    player x and o

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 "use strict";
 
+// Selecting HTML element by class and IDs
 const gameStatus = document.querySelector(".status");
 const btns = document.querySelectorAll(".btn");
 console.log(btns);
+
+// Variables & Arrays declaration
+const xoArray = ["X", "O"];
+const playerX = "X"
+const playerO = "O"
+let activePlayer = "";
+
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
